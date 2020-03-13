909140744e7ae925b8eec057870c9f7a934cea9e
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-13 16:38:22 +0100

    Created main image cmp and rendered image,text&title

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-undef */
 import React,{useEffect, useState} from 'react'
 // import { FaCode } from "react-icons/fa";
-import {API_URL, API_KEY} from '../../Config'
+import {API_URL, API_KEY, IMAGE_URL} from '../../Config'
 import {Typography, Row} from 'antd'
+import MainImage from './Sections/MainImage'
 const {Title} = Typography 
 
 function LandingPage() {
@@ -22,23 +23,16 @@ function LandingPage() {
 
     return (
       <div style={{width:'100%', margin:0 }}>
-     {/* Main movie image*/}
-     
-     <div style={{background: 
-        `linear-gradient(to bottom, rgba(0,0,0,0)39%, rgba(0,0,0,0)41%, rgba(0,0,0,0.65)100%), url(), #1c1c1c`,
-     height:'500px',
-     backgroundSize:'100%, cover',
-     backgroundPosition:'center, center',
-     width:'100%',
-     position:'relative'}}>
-     </div>
 
-     <div>
-     <div style={{position:'absolute', maxWidth:'500px', bottom:'2rem', marginLeft:'2rem'}}>
-     <Title style={{color:'white'}} level={2}> Title</Title>
-     <p style={{color:'white', fontSize:'1rem'}}>text</p>
-     </div>
-     </div>
+     {/* Main movie image*/}
+     { Movies[0] && 
+      <MainImage image={`${IMAGE_URL}/w1280${Movies[0].backdrop_path}`}
+      title={Movies[0].original_title}
+      text={Movies[0].overview}
+      />
+    }
+     
+   
 
        {/* Body*/}
        <div style={{width:'85%', margin:'1rem auto'}}>
